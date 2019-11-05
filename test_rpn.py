37ee04da75b616f7c2b8d4345a3242b18a3133df
Brendan Flannagan <bflann@umich.edu>
2019-11-04 22:58:59 -0500

    Added test for carrot

--- test_rpn.py
+++ test_rpn.py
@@ -14,9 +14,14 @@ class TestBasics(unittest.TestCase):
     def test_bad_input(self):
         with self.assertRaises(TypeError):
             rpn.calculate('1 2 3 +')
+
+    def test_carrot(self):
+        result = rpn.calculate('2 2 ^')
+        self.assertEqual(4, result)
             
 if __name__ == '__main__':
     test = TestBasics()
     test.test_add()
     test.test_bad_input()
     test.test_sub()
+    test.test_carrot()
